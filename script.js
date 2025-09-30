b4c68ab27c11936f695032be5f52796a93554a18
joseph0017 <josephogiku96@gmail.com>
2025-09-30 08:37:56 +0100

    changed url api to dummy json

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-const quote = document.getElementById("quote");
-const author = document.getElementById("author");
+const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author");
 const quoteBtn = document.getElementById("new-quote");
-const api_url = "https://api.quotable.io/random";
+const api_url = "https://dummyjson.com/quotes";
 
 async function getQuote(url) {
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
-  quote.innerHTML = data.content;
-  author.innerHTML = data.author;
+
+  // pick a random quote from the array
+  const randomIndex = Math.floor(Math.random() * data.quotes.length);
+  const randomQuote = data.quotes[randomIndex];
+
+  // update UI
+  quoteEl.innerText = randomQuote.quote;
+  authorEl.innerText = `— ${randomQuote.author}`;
 }
 
+// load first quote on page load
 getQuote(api_url);
 
+// get new quote on button click
 quoteBtn.addEventListener("click", () => {
   getQuote(api_url);
 });
