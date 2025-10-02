ba43394dc11eccdb0558d9eb2208b6fc85e840eb
BellaAkins <sarabellaakins@gmail.com>
2025-10-02 01:11:48 +0100

    m

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 const quoteBtn = document.getElementById("new-quote");
+const postBtn = document.getElementById("post");
 const api_url = "https://api.quotable.io/random";
 
 async function getQuote(url) {
@@ -16,3 +17,14 @@ getQuote(api_url);
 quoteBtn.addEventListener("click", () => {
   getQuote(api_url);
 });
+postBtn.addEventListener("click", () => {
+  tweet();
+});
+
+function tweet() {
+  window.open(
+    "https://twitter.com/intent/tweet",
+    "Tweet window",
+    "width=600, height=300"
+  );
+}
